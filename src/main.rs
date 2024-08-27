dd59bfab512fce70c90532726991b56b682673ff
Kim <joakim.linna@edu.futuregames.nu>
2024-08-27 21:29:44 +0200

    i give up. make a wrapper or something idgaf, just dont use recursiveness AT ALL

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,13 @@ use patina_engine::prelude::*;
 fn main() {
     let player = Actor::new("Player");
     println!("{}", player);
+    let player_transform = player.transform();
     
     let player_model = Actor::new("Model");
     let model_transform = player_model.transform();
     model_transform.borrow_mut().set_parent(Some(player.transform()));
+    player_transform.borrow_mut().add_child(model_transform.clone());
+    
 
     let model_parent = model_transform.borrow().parent();
     println!("{:?}", model_parent);
--- src/transform.rs
+++ src/transform.rs
@@ -1,15 +1,35 @@
-use std::{cell::RefCell, rc::Rc};
-
+use std::{cell::RefCell, ops::Index, rc::Rc};
 use glam::{Quat, Vec3};
+
+// ok so here's the problem:
+// parent needs to be None or Some - Option
+// if it is some, it needs a reference to a transform reference - RefCell
+// multiple transforms may share the same parent - Rc
 
 #[derive(Debug, Default)]
 pub struct Transform {
     pub position: Vec3,
     pub rotation: Quat,
     pub scale: Vec3,
-    parent: Option<Rc<RefCell<Transform>>>
+    parent: Option<Rc<RefCell<Transform>>>,
+    children: Vec<Rc<RefCell<Transform>>>,
 }
+impl PartialEq for Transform {
+    fn eq(&self, other: &Self) -> bool {
+        self.position == other.position && self.rotation == other.rotation && self.scale == other.scale && self.parent == other.parent && self.children == other.children
+    }
+}
+
 impl Transform {
+    pub fn parent(&self) -> Option<Rc<RefCell<Transform>>> {
+        match &self.parent {
+            Some(parent_rc) => {
+                Some(parent_rc.clone())
+            },
+            _ => None,
+        }
+    }
+
     pub fn set_parent(&mut self, parent: Option<Rc<RefCell<Transform>>>) {
         self.parent = match &parent {
             Some(parent_rc) => {
@@ -19,17 +39,17 @@ impl Transform {
         }
     }
 
-    pub fn parent(&self) -> Option<Rc<RefCell<Transform>>> {
-        match &self.parent {
-            Some(parent_rc) => {
-                Some(parent_rc.clone())
-            },
-            _ => None,
+    pub fn children(&self) -> &Vec<Rc<RefCell<Transform>>> {
+        &self.children
+    }
+
+    pub fn add_child(&mut self, child: Rc<RefCell<Transform>>) {
+        if self.children.contains(&child) {
+            return;
         }
+
+        //todo! set parent of child to self
+
+        self.children.push(child.clone())
     }
 }
-
-// ok so here's the problem:
-// parent needs to be None or Some - Option
-// if it is some, it needs a reference to a transform reference - RefCell
-// multiple transforms may share the same parent - Rc
